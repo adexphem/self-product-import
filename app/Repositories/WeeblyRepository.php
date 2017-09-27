70803cc33c480c50bb37826037fbdcf6f162a5c5
adexphem <adexphem4real@gmail.com>
2017-09-27 23:10:21 +0100

    removing redisClient reference2

--- app/Repositories/WeeblyRepository.php
+++ app/Repositories/WeeblyRepository.php
@@ -158,8 +158,8 @@ class WeeblyRepository {
      * @return GuzzleHttpResponse            response from Weebly after update
      */
     public function updateDashboardCard (GuzzleClient $guzzleClient, Site $site, string $dashboardCardId, array $cardUpdate) : GuzzleHttpResponse {
-        $endpoint = self::$apiHost. "/v1/user/sites/" .$site->weebly_site_id. "/cards/". $dashboardCardId;
-
+        $endpoint = self::$apiHost. "/v1/user/sites/" .$site->weebly_site_id. "/cards/" .$dashboardCardId;
+dd($cardUpdate);
         return $guzzleClient->request("PATCH", $endpoint, [
             "headers" => [
                 "Accept" => self::WEEBLY_ACCEPT_HEADER,
